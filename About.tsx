dddf3d7c618b186172349727732cb47146d6159c
diyawalunj <diyawalunj@gmail.com>
2026-03-24 15:48:37 +0530

    Update image paths for founders in About.tsx

--- About.tsx
+++ About.tsx
@@ -4,28 +4,28 @@ import { Target, Eye, Heart, Shield, Flag, Mountain, Medal, Dumbbell, Globe, Quo
 const FOUNDERS = [
   {
     name: 'Abhijeet Gaikwad',
-    image: '/abhi.jpg',
+    image: 'public/abhi.jpg',
     qualification: 'NDA Qualified',
     degrees: ['B.Tech in AI & Data Science', 'B.A. (Hons.) Political Science'],
     squadron: 'NCC-1 MAH AIR SQN'
   },
   {
     name: 'Aditya Sahane',
-    image: '/aditya.jpg',
+    image: 'public/aditya.jpg',
     qualification: 'NDA Qualified',
     degrees: ['B.Tech in Computer Science Engineering'],
     squadron: 'NCC-3 MAH AIR SQN'
   },
   {
     name: 'Rishikesh Muthal',
-    image: '/rishi.jpg',
+    image: 'public/rishi.jpg',
     qualification: 'NDA Qualified',
     degrees: ['B.Tech in AI & Data Science'],
     squadron: 'NCC-1 MAH AIR SQN'
   },
   {
     name: 'Vedant Jadhav',
-    image: '/vedant.jpg',
+    image: 'public/vedant.jpg',
     qualification: 'CDS Qualified',
     degrees: ['B.E. Information Technology'],
     squadron: 'NCC-1 MAH AIR SQN'
